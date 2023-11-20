ecdca1bc7cd3fd1ba1ecccf72d0cce84efef0a81
Nikolas Nakamura <nikolas.shiro.nakamura@gmail.com>
2023-11-20 22:28:28 +0900

    fetch of real restaurants of our real location

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   const [places, setPlaces] = useState([]);
 
   const [coordinates, setCoordinates] = useState({});
-  const [bounds, setBounds] = useState(null);
+  const [bounds, setBounds] = useState({});
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -21,8 +21,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    console.log(coordinates, bounds);
-    getPlacesData().then((data) => {
+    getPlacesData(bounds.sw, bounds.ne).then((data) => {
       console.log(data);
       setPlaces(data);
     });
