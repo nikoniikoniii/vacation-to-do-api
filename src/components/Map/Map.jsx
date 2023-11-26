179d8f2519630177f9ba76729a77eab7fef63d3f
Nikolas Nakamura <nikolas.shiro.nakamura@gmail.com>
2023-11-26 23:41:13 +0900

    pings on the map added

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,10 +5,11 @@ import locationOnOutlined from "@material-ui/icons/LocationOnOutlined";
 import { Rating } from "@material-ui/lab";
 
 import useStyles from "./styles";
+import LocationOnOutlined from "@material-ui/icons/LocationOnOutlined";
 
-const Map = ({ setCoordinates, setBounds, coordinates }) => {
+const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
   const classes = useStyles();
-  const isMobile = useMediaQuery("(min-width:600px)");
+  const isDesktop = useMediaQuery("(min-width:600px)");
 
   return (
     <div className={classes.mapContainer}>
@@ -25,7 +26,42 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
         //onChildClick={``}
-      />
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isDesktop ? (
+              <LocationOnOutlined
+                color="primary"
+                fontSize="large"
+              ></LocationOnOutlined>
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.Typography}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                  }
+                  alt={place.name}
+                />
+              </Paper>
+            )}
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   );
 };
